6daf2b16af8804b7beb9ccbf16c9a4df017e1527
gustavoscarvalho <gustavoscarvalho@yahoo.com.br>
2015-01-24 21:06:41 -0200

    Add Project

---
+++ vraptor-with-legacy-servlet/src/br/com/routes/MyRoutesParser.java
@@ -0,0 +1,26 @@
+package br.com.routes;
+
+import java.lang.reflect.Method;
+
+import br.com.caelum.vraptor.http.route.PathAnnotationRoutesParser;
+import br.com.caelum.vraptor.http.route.Router;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
+@ApplicationScoped
+public class MyRoutesParser extends PathAnnotationRoutesParser {
+
+    public MyRoutesParser(Router router) {
+		super(router);	
+	}
+
+	@Override
+    protected String[] getURIsFor(Method javaMethod, Class<?> type) {
+      String[] uris = super.getURIsFor(javaMethod, type);
+      for(int i = 0; i < uris.length; i++) {
+           uris[i] = "/rest" + uris[i];
+      }
+      return uris;
+    }
+}
